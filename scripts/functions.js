433c4a87d090a56a12a081338e17c200780e84ef
Erik Anderson <erik@Eriks-MacBook-Pro.local>
2020-12-15 22:54:28 -0500

    page structure and projects added

---
+++ scripts/functions.js
@@ -0,0 +1,20 @@
+console.log('liv');
+document.getElementById('slide-1').addEventListener('click', function() {
+    console.log('sup');
+    location.href = 'one.html'
+}, false);
+
+document.getElementById('slide-2').addEventListener('click', function() {
+    console.log('sup');
+    location.href = 'two.html'
+}, false);
+
+document.getElementById('slide-3').addEventListener('click', function() {
+    console.log('sup');
+    location.href = 'three.html'
+}, false);
+
+document.getElementById('slide-4').addEventListener('click', function() {
+    console.log('sup');
+    location.href = 'four.html'
+}, false);
